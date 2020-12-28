4fb40f539652d61a9245c50d1fd6632d97a4cd88
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2020-12-29 00:22:51 +0300

    Update Upcoming.js

--- ChurchMaster/church-submaster/src/Navigators/Upcoming.js
+++ ChurchMaster/church-submaster/src/Navigators/Upcoming.js
@@ -1,10 +1,11 @@
 import React from 'react' ;
+import './style.css';
 
 function Upcoming(){
 return(
-
+  <div className="containerBlue">
    <h1>Upcoming events 2021</h1>
-
+  </div>
   )
 }
 export default Upcoming
