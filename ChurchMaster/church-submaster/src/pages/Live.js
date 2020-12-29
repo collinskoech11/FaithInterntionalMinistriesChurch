f14d79ef07887ca512945fac3d5d872e3ade23e7
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2020-12-29 16:31:10 +0300

    Update Live.js

--- ChurchMaster/church-submaster/src/pages/Live.js
+++ ChurchMaster/church-submaster/src/pages/Live.js
@@ -1,10 +1,17 @@
 
 import React from 'react';
 
+import YouTubePlayer from “react-player/lib/players/YouTube”;
+
+
 
 function Live() {
     return (
-        
+        <div className="video">
+            <YouTubePlayer
+               url='https://www.youtube.com/watch?v=d46Azg3Pm4c'
+             />
+        </div>
     )
 }
 
