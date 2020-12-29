49cd3631c21eb3fbe1200a686e9d260e94a8b099
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2020-12-29 10:42:58 +0300

    Update Chairman.js

--- ChurchMaster/church-submaster/src/pages/SuperProfiles/Chairman.js
+++ ChurchMaster/church-submaster/src/pages/SuperProfiles/Chairman.js
@@ -1,1 +1,24 @@
 
+import React from 'react';
+import './styles.css';
+
+function Chairman() {
+    return (
+        <div className="containerBlue">
+            <div className="profile-title">
+
+                <h2>Word from our spiritual father</h2>
+                <h1 className="head">Bishop Jimmy Bett</h1>
+            
+            </div>
+            <div className="profile-img">
+                <img src="https://source.unsplash.com/400x200/?Bishop" alt=""/>
+            </div>
+            <div className="profile-desc">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptates dicta sed doloremque et non esse modi saepe nemo mollitia atque quo accusamus odio quam, consectetur libero, commodi impedit animi!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Chairman
