f83d3169f2073f69a5a1af6add68c3837e011456
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2020-12-29 16:15:36 +0300

    Update Upcoming.js

--- ChurchMaster/church-submaster/src/Navigators/Upcoming.js
+++ ChurchMaster/church-submaster/src/Navigators/Upcoming.js
@@ -10,7 +10,7 @@ return(
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptates dicta sed doloremque et non esse modi saepe nemo mollitia atque quo accusamus odio quam, consectetur libero, commodi impedit animi!</p>
    </div>
    <div className="plot">
-        <h2 className="head">Upcoming plot purchase plan</h2>
+        <h2 className="head">End year prayer service </h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptates dicta sed doloremque et non esse modi saepe nemo mollitia atque quo accusamus odio quam, consectetur libero, commodi impedit animi!</p>
    </div>
    <div className="plot">
