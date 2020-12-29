dd59427401f37e4198ac1f869687722f484be86f
Collins Koech <collinskoechck34@gmail.com>
2020-12-29 19:39:55 +0300

    update

--- ChurchMaster/church-submaster/src/pages/JustFm.js
+++ ChurchMaster/church-submaster/src/pages/JustFm.js
@@ -4,7 +4,7 @@ import { FacebookProvider, Feed } from 'react-facebook';
 function JustFm() {
     return (
         <FacebookProvider appId="123456789">
-        <Feed link="https://www.facebook.com">
+        <Feed link="https://www.facebook.com/">
           {({ handleClick }) => (
             <button type="button" onClick={handleClick}>Share on Feed</button>
           )}
--- GikombaOnline/backend/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/GikombaOnline',
-    JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
-}
