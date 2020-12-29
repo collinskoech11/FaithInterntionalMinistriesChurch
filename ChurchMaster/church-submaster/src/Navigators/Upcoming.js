98f81691b3bb27423d3a509ccc4baf96730489a5
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2020-12-29 15:54:45 +0300

    Update Upcoming.js

--- ChurchMaster/church-submaster/src/Navigators/Upcoming.js
+++ ChurchMaster/church-submaster/src/Navigators/Upcoming.js
@@ -5,6 +5,10 @@ function Upcoming(){
 return(
   <div className="containerBlue">
    <h1>Upcoming events 2021</h1>
+   <div className="plot">
+        <h2 className="head">Upcoming plot purchase plan</h2>
+        <p></p>
+   </div>
   </div>
   )
 }
