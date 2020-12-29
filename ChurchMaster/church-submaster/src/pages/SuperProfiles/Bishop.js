6073b7c40d96b643f0ef4dfa11aab0674faa1d51
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2020-12-29 09:47:36 +0300

    Update Bishop.js

--- ChurchMaster/church-submaster/src/pages/SuperProfiles/Bishop.js
+++ ChurchMaster/church-submaster/src/pages/SuperProfiles/Bishop.js
@@ -4,12 +4,16 @@ import './styles.css';
 function Bishop() {
     return (
         <div className="containerBlue">
+            <div className="profile-title">
+
+                <h2>Word from our spiritual father</h2>
+                <h1 className="head">Bishop Jimmy Bett</h1>
+            
+            </div>
             <div className="profile-img">
                 <img src="https://source.unsplash.com/400x200/?Bishop" alt=""/>
             </div>
             <div className="profile-desc">
-                <h2>Word from our spiritual father</h2>
-                <h1 className="head">Bishop Jimmy Bett</h1>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptates dicta sed doloremque et non esse modi saepe nemo mollitia atque quo accusamus odio quam, consectetur libero, commodi impedit animi!</p>
             </div>
         </div>
