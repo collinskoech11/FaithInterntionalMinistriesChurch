037b138e1f69a0248faa79f8dd18a7be3d070b3e
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2020-12-29 16:00:59 +0300

    Update Upcoming.js

--- ChurchMaster/church-submaster/src/Navigators/Upcoming.js
+++ ChurchMaster/church-submaster/src/Navigators/Upcoming.js
@@ -7,14 +7,7 @@ return(
    <h1>Upcoming events 2021</h1>
    <div className="plot">
         <h2 className="head">Upcoming plot purchase plan</h2>
-        <p>    <h1 className="head">Bishop Jimmy Bett</h1>
-            
-            </div>
-            <div className="profile-img">
-                <img src="https://source.unsplash.com/400x200/?Bishop" alt=""/>
-            </div>
-            <div className="profile-desc">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptates dicta sed doloremque et non esse modi saepe nemo mollitia atque quo accusamus odio quam, consectetur libero, commodi impedit animi!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptates dicta sed doloremque et non esse modi saepe nemo mollitia atque quo accusamus odio quam, consectetur libero, commodi impedit animi!</p>
    </div>
   </div>
   )
