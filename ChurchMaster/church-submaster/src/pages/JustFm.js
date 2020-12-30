22e8b26751c399c0c99f1e571afec2273bee3f1f
Collins Koech <collinskoechck34@gmail.com>
2020-12-30 18:48:38 +0300

    data entry

--- ChurchMaster/church-submaster/src/pages/JustFm.js
+++ ChurchMaster/church-submaster/src/pages/JustFm.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './style.css';
+import just from './img/just.jpg';
 
 function JustFm() {
   return (
     <div className="justCon">
       <h1>Just f.m radio</h1> <h3>98.9 fm</h3>
+      <img src={just} alt="just fm header image"/>
     </div>
   )
 }
--- ChurchMaster/church-submaster/src/pages/branches/Litein.js
+++ ChurchMaster/church-submaster/src/pages/branches/Litein.js
@@ -3,6 +3,7 @@ import './style.css';
 
 function Litein() {
     return (
+        <>
         <div className="b-head">
             <div className="profile-img">
                 <img src="https://source.unsplash.com/400x200/?Bishop" alt="Branch head profile"/>
@@ -11,6 +12,152 @@ function Litein() {
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea architecto consequatur exercitationem aspernatur quo ad voluptates reprehenderit animi doloremque debitis sint saepe quaerat odio nisi, ipsa at, neque veritatis modi.</p>
             </div>
         </div>
+
+        <div className="sub-branch-title">
+            <h2>Our Sub branches</h2>
+        </div>
+        <div className="sub-branch">
+            <div className="data">
+                <h4>Church Name</h4>
+            </div>
+            <div className="data">
+                <h4>Pastors Contact</h4>
+            </div>
+            <div className="data">
+                <h4>Pastors Name</h4>
+            </div>
+            <div className="data">
+                <h4>Number of Members</h4>
+            </div>
+            <div className="data">
+                <h4>Plot /Building</h4>
+            </div>
+            <div className="data">
+                <h4>Project</h4>
+            </div>    
+        </div>
+        <div className="sub-branch">
+            <div className="data">
+                <p>Litein</p>
+            </div>
+            <div className="data">
+                <p>Rev Koskei</p>
+            </div>
+            <div className="data">
+                <p>0721723316</p>
+            </div>
+            <div className="data">
+                <p>  </p>
+            </div>
+            <div className="data">
+                <p>  </p>
+            </div>
+            <div className="data">
+                <p>  </p>
+            </div>    
+        </div>
+        <div className="sub-branch">
+            <div className="data">
+                <p>Roret</p>
+            </div>
+            <div className="data">
+                <p>  </p>
+            </div>
+            <div className="data">
+                <p>0715300953</p>
+            </div>
+            <div className="data">
+                <p></p>
+            </div>
+            <div className="data">
+                <p></p>
+            </div>
+            <div className="data">
+                <p></p>
+            </div>    
+        </div>
+        <div className="sub-branch">
+                <div className="data">
+                    <p>Chelilis</p>
+                </div>
+                <div className="data">
+                    <p>Pstr Ann Rotich</p>
+                </div>
+                <div className="data">
+                    <p>0722636617</p>
+                </div>
+                <div className="data">
+                    <p></p>
+                </div>
+                <div className="data">
+                    <p></p>
+                </div>
+                <div className="data">
+                    <p></p>
+                </div> 
+            </div>
+            <div className="sub-branch">
+            
+                <div className="data">
+                    <p>Kapminjewet</p>
+                </div>
+                <div className="data">
+                    <p>Pstr Nixon Tonui</p>
+                </div>
+                <div className="data">
+                    <p>0715065622</p>
+                </div>
+                <div className="data">
+                    <p></p>
+                </div>
+                <div className="data">
+                    <p></p>
+                </div>
+                <div className="data">
+                    <p></p>
+                </div>    
+            </div>
+            <div className="sub-branch">
+                <div className="data">
+                    <p>Boito</p>
+                </div>
+                <div className="data">
+                    <p>Pstr R.Langat</p>
+                </div>
+                <div className="data">
+                    <p>0728695684</p>
+                </div>
+                <div className="data">
+                    <p></p>
+                </div>
+                <div className="data">
+                    <p></p>
+                </div>
+                <div className="data">
+                    <p></p>
+                </div>    
+            </div>
+            <div className="sub-branch">
+                <div className="data">
+                    <p>Chepwagan</p>
+                </div>
+                <div className="data">
+                    <p>Pstr Towett</p>
+                </div>
+                <div className="data">
+                    <p>0729266239</p>
+                </div>
+                <div className="data">
+                    <p></p>
+                </div>
+                <div className="data">
+                    <p></p>
+                </div>
+                <div className="data">
+                    <p></p>
+                </div>    
+            </div>      
+        </>
     )
 }
 
