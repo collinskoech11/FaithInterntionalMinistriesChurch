9db833e83e99d3f24ac2acead84c628f386bd658
Collins Koech <collinskoechck34@gmail.com>
2021-01-01 00:25:48 +0300

    update

--- ChurchMaster/church-submaster/src/pages/Live.js
+++ ChurchMaster/church-submaster/src/pages/Live.js
@@ -6,12 +6,24 @@ import './style.css';
 
 function Live() {
     return (
+      <>
         <div className="video">
-      <h3>Embed YouTube video - <a href="https://www.cluemediator.com">Clue Mediator</a></h3>
-      <ReactPlayer
-        url="https://www.youtube.com/watch?v=UVCP4bKy9Iw"
-      />
-    </div>
+          <h3>Latest Church Service</h3>
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=CGXh0uBEtBc"
+          />
+        </div>
+        <div className="video">
+          <h3>More Live Church Services</h3>
+          <ReactPlayer url="https://www.youtube.com/watch?v=hSj0e4rxqtA"/>
+        </div>
+        <div className="video">
+          <ReactPlayer url="https://www.youtube.com/watch?v=k8bwVZI3uGE"/>
+        </div>
+        <div className="video">
+          <ReactPlayer url="https://www.youtube.com/watch?v=Qt7JuWOVTWw"/>
+        </div>
+        </>
     )
 }
 
