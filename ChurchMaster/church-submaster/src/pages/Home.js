866c087c08a5ab59134e0ab36bae7575bf45e21e
Collins Koech <collinskoechck34@gmail.com>
2020-12-31 19:30:55 +0300

    update

--- ChurchMaster/church-submaster/src/pages/Home.js
+++ ChurchMaster/church-submaster/src/pages/Home.js
@@ -22,14 +22,14 @@ export default function Home() {
                         </div>
                         <div className="block">
                             <h4>
-                            <CountUp start={0} end={100} />
+                            <CountUp start={0} end={1000} />
                             </h4>
                         </div>
                         <div className="block">
                             <h3>Total Members :</h3>
                         </div>
                         <div className="block">
-                            <h4><CountUp start={0} end={100} /></h4>
+                            <h4><CountUp start={0} end={100000} /></h4>
                         </div>
                     </div>
                 </div>
