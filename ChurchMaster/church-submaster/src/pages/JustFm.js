e71a8cc992ff063f50a62a05f78f8b49fcce5e3d
Collins Koech <collinskoechck34@gmail.com>
2021-01-05 13:52:16 +0300

    text animation

--- ChurchMaster/church-submaster/src/pages/JustFm.js
+++ ChurchMaster/church-submaster/src/pages/JustFm.js
@@ -4,10 +4,15 @@ import just from './img/just.jpg';
 
 function JustFm() {
   return (
+    <>
     <div className="justCon">
       <h1>Just f.m radio</h1> <h3>98.9 fm</h3>
       <img src={just} alt="just fm header image"/>
     </div>
+    <div className="title">
+      <h1><Sparkles><Strong>Live Radio Streaming Coming soon</Strong></Sparkles> </h1>
+    </div>
+    </>
   )
 }
 
