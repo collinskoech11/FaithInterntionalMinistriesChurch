3891a4571cd121b3b82ad6fd215ef1deaa6b01de
Collins Koech <collinskoechck34@gmail.com>
2021-01-05 23:57:30 +0300

    update

--- ChurchMaster/church-submaster/src/pages/Location.js
+++ ChurchMaster/church-submaster/src/pages/Location.js
@@ -8,7 +8,7 @@ export class Location extends Component {
 
    
     <div id="map-container-google-4" class="z-depth-1-half map-container-10" >
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.7359864172404!2d35.28066581469809!3d-0.36808763541737993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182a57399f1bda5b%3A0x70ee2b689e9303df!2sFaith%20Ministries%20Church%20International%20Office!5e0!3m2!1sen!2ske!4v1609154991727!5m2!1sen!2ske" width="100%" height="450" frameborder="0"  allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.7359864172404!2d35.28066581469809!3d-0.36808763541737993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182a57399f1bda5b%3A0x70ee2b689e9303df!2sFaith%20Ministries%20Church%20International%20Office!5e0!3m2!1sen!2ske!4v1609154991727!5m2!1sen!2ske" width="100%" height="450" frameborder="0"  allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
     </div>
 
   </div>
