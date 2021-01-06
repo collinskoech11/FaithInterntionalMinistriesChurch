08142ec80fac8de046afbdbbcc538124dbcebae5
Collins Koech <collinskoechck34@gmail.com>
2021-01-06 10:43:43 +0300

    project update

--- ChurchMaster/church-submaster/src/pages/ContactDev.js
+++ ChurchMaster/church-submaster/src/pages/ContactDev.js
@@ -6,18 +6,18 @@ function ContactDev() {
        <>
         <div className="dev">
             <div className="dev-prof">
-                <img src="" alt=""/>
+                <img src="https://source.unsplash.com/400x300/?Church" alt=""/>
             </div>
             <div className="dev-desc">
-                <p></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur eaque nihil corrupti dolorum, assumenda incidunt enim veritatis, qui expedita, odio fuga nam rerum ducimus quam! Nihil sed nam nisi at!</p>
             </div>
         </div>
         <div className="dev">
             <div className="dev-prof">
-                <img src="" alt=""/>
+                <img src="https://source.unsplash.com/400x300/?Church" alt=""/>
             </div>
             <div className="dev-desc">
-                <p></p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque iusto, eveniet quaerat laudantium maxime consequatur maiores sit doloremque magnam? Aliquid iusto magnam quae unde blanditiis quam rerum ipsa sequi obcaecati!</p>
             </div>
         </div>
        </>
