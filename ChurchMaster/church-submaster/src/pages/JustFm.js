28c2648fd1e388b8f302752f4f313c64b73c6d0f
Collins Koech <collinskoechck34@gmail.com>
2021-01-06 13:13:56 +0300

    update

--- ChurchMaster/church-submaster/src/pages/JustFm.js
+++ ChurchMaster/church-submaster/src/pages/JustFm.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './style.css';
 import just from './img/just.jpg';
+import Twitter from '../Navigators/Twitter';
 
 function JustFm() {
   return (
     <>
     <div className="justCon">
-      <h1>Just f.m radio</h1> <h3>98.9 fm</h3>
+      <h1 >Just f.m radio</h1> <h3>98.9 fm</h3>
       <img src={just} alt="just fm header image"/>
     </div>
     <div className="title">
       <h1>Live Radio Streaming Coming soon </h1>
+    </div>
+    <div className="container">
+      <Twitter/>
     </div>
     </>
   )
