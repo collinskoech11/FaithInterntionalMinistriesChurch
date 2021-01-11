8865d129b0087b644b892394e28b6d52163046db
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-01-11 15:25:21 +0300

    Update Olenguruone.js

--- ChurchMaster/church-submaster/src/pages/branches/Olenguruone.js
+++ ChurchMaster/church-submaster/src/pages/branches/Olenguruone.js
@@ -230,6 +230,20 @@ function Olenguruone() {
                 <p></p>
             </div>  
         </div>
+        <div className="sub-branch">
+            <div className="data">
+                <p>Total</p>
+            </div>
+            <div className="data">
+                <p></p>
+            </div>
+            <div className="data">
+                <p>14</p>
+            </div>
+            <div className="data">
+                <p></p>
+            </div>  
+        </div>
     </>
     )
 }
