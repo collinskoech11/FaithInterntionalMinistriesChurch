8c2159915515c196767d126832b16cb86a2fc3fb
Collins Koech <collinskoechck34@gmail.com>
2021-01-12 12:58:34 +0300

    update

--- ChurchMaster/church-submaster/src/pages/Live.js
+++ ChurchMaster/church-submaster/src/pages/Live.js
@@ -8,27 +8,38 @@ function Live() {
     return (
       <>
       <div className="row">
-      <div className="col-lg-6">
-          <div className="col-lg-6 col-md-6">
-            <h3>Latest Church Service</h3>
-            <ReactPlayer
-              url="https://www.youtube.com/watch?v=CGXh0uBEtBc"
-            />
+
+
+        <div className="col-lg-6" style={{marginBottom:'20px'}}>
+            <div className="col-lg-6 col-md-6">
+              <h3>Latest Church Service</h3>
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=CGXh0uBEtBc"
+              />
+            </div>
           </div>
-          <div className="col-lg-6 col-md-6">
-            <h3>More Live Church Services</h3>
-            <ReactPlayer url="https://www.youtube.com/watch?v=hSj0e4rxqtA"/>
+        
+
+          <div className="col-lg-6" style={{marginBottom:'20px'}}>
+            <div className="col-lg-6 col-md-6">
+              <h3>More Live Church Services</h3>
+              <ReactPlayer url="https://www.youtube.com/watch?v=hSj0e4rxqtA"/>
+            </div>
           </div>
-          </div>
-          <div className="col-lg-6">
+
+        
+          <div className="col-lg-6" style={{marginBottom:'20px'}}>
             <div className="col-lg-6 col-md-6">
               <ReactPlayer url="https://www.youtube.com/watch?v=k8bwVZI3uGE"/>
-            </div>
+            </div></div>
+
+            <div className="col-lg-6" style={{marginBottom:'20px'}}>
             <div className="col-lg-6 col-md-6">
               <ReactPlayer url="https://www.youtube.com/watch?v=Qt7JuWOVTWw"/>
             </div>
         </div>
-       </div> </>
+       </div> 
+       </>
     )
 }
 
