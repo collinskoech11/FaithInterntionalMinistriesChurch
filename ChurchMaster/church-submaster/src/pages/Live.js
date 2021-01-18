845085f88008b248ee2529a93c8c76b83e65db03
Collins Koech <collinskoechck34@gmail.com>
2021-01-18 12:52:55 +0300

    content update

--- ChurchMaster/church-submaster/src/pages/Live.js
+++ ChurchMaster/church-submaster/src/pages/Live.js
@@ -14,7 +14,7 @@ function Live() {
             <div className="col-lg-6 col-md-6">
               <h3>Latest Church Service</h3>
               <ReactPlayer
-                url="https://www.youtube.com/watch?v=H2wmR0dZv2w"
+                url="https://www.youtube.com/watch?v=jr21cYk9zUE"
               />
             </div>
           </div>
@@ -23,6 +23,12 @@ function Live() {
           <div className="col-lg-6" style={{marginBottom:'20px'}}>
             <div className="col-lg-6 col-md-6">
               <h3>More Live Church Services</h3>
+              <ReactPlayer url="https://www.youtube.com/watch?v=H2wmR0dZv2w"/>
+            </div>
+          </div>
+
+          <div className="col-lg-6" style={{marginBottom:'20px'}}>
+            <div className="col-lg-6 col-md-6">
               <ReactPlayer url="https://www.youtube.com/watch?v=CGXh0uBEtBc"/>
             </div>
           </div>
