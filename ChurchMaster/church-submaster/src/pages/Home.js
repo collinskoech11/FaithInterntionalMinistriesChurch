97c06ccc1623a444c1424f2af1a32d3579bc9e55
collinskoech11 <collinskoechck34@gmail.com>
2021-02-04 12:26:27 +0300

    update

--- ChurchMaster/church-submaster/src/pages/Home.js
+++ ChurchMaster/church-submaster/src/pages/Home.js
@@ -7,92 +7,74 @@ import './style.css';
 import Twitter from '../Navigators/Twitter';
 import CountUp from 'react-countup';
 import {Animated} from 'react-animated-css';
-import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, Move, MoveIn, MoveOut, Sticky, StickyIn, ZoomIn } from "react-scroll-motion";
+
+
 
 
 export default function Home() {
-
-   
-
     return (
         <div>
-            <ScrollContainer>
-                <ScrollPage page={0}>
-                    <Animated animationIn="bounceIn" animationOut="fadeOut" isVisible={true}>
-                        <div className="container first">
-                        <div className="f-block"><CarouselPage/></div>
-                        <div className="f-block">
-                            <div className="mpesa">
-                                <div className="block">
-                                    <h3>No of Satelite Branches</h3>
+            <Animated animationIn="bounceIn" animationOut="fadeOut" isVisible={true}>
+            <div className="container first">
+                <div className="f-block"><CarouselPage/></div>
+                <div className="f-block">
+                    <div className="mpesa">
+                        <div className="block">
+                            <h3>No of Satelite Branches</h3>
+                        </div>
+                        <div className="block">
+                            <h3>
+                            <CountUp start={0} end={151} delay={0} duration={5}>
+                            {({ countUpRef }) => (
+                                <div>
+                                <span ref={countUpRef} />
                                 </div>
-                                <div className="block">
-                                    <h3>
-                                    <CountUp start={0} end={151} delay={0} duration={5}>
-                                    {({ countUpRef }) => (
-                                        <div>
-                                        <span ref={countUpRef} />
-                                        </div>
-                                    )}
-                                    </CountUp>
-                                    </h3>
+                            )}
+                            </CountUp>
+                            </h3>
+                        </div>
+                        <div className="block">
+                            <h3>Total Members :</h3>
+                        </div>
+                        <div className="block">
+                            <h3><CountUp start={0} end={7550} delay={0}  duration={5} >
+                            {({ countUpRef }) => (
+                                <div>
+                                <span ref={countUpRef} />
                                 </div>
-                                <div className="block">
-                                    <h3>Total Members :</h3>
-                                </div>
-                                <div className="block">
-                                    <h3><CountUp start={0} end={7550} delay={0}  duration={5} >
-                                    {({ countUpRef }) => (
-                                        <div>
-                                        <span ref={countUpRef} />
-                                        </div>
-                                    )}
-                                    </CountUp></h3>
-                                </div>
-                            </div>
+                            )}
+                            </CountUp></h3>
                         </div>
                     </div>
-                    </Animated>
-                </ScrollPage>
-                <ScrollPage page={1}>
-                    <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true} animationInDelay={6} animationInDuration={2000}>
-                        <div className="container">
-                            <Bishop/>
-                        </div>
-                    </Animated>
-                </ScrollPage>
-                <ScrollPage page={2}>
-                    <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200))}>
-                        <div className="container">
-                            <h1>Join us Sunday mornings as we gather online</h1>
-                            <p>The Faith INternational Ministries Church wants to do everything we can to ensure that our communities are taking the necessary precautions to slow the spread of COVID-19. At the same time, we want to make it possible for everyone to experience the fellowship and Biblical teaching that keeps our faith strong, which is more important now than ever.
-                                With this in mind, we are ready to offer a series of special live stream worship services, Sundays at 11am GMT+3. These will take the place of in-person services for all of our Bay Area locations. We hope you’ll join us online!</p>
-                            <br/><br/><a href="/Live">--- join us live</a>
-                            <img src="https://source.unsplash.com/400x300/?Church" alt=""/>
-                        </div>
-                    </Animator>
-                </ScrollPage>
-                <ScrollPage page={3}>
-                    <div className="container">
-                        <Twitter/>
-                    </div>
-                </ScrollPage>
-                <ScrollPage page={4}>
-                    <div className="container">
-                        <Chairman/>
-                    </div>
-                </ScrollPage>
-                <ScrollPage page={5}>
-                    <div className="container">
-                        <Upcoming/>
-                    </div>
-                </ScrollPage>
-                <ScrollPage page={6}>
-                    <div className="container">
-                        <Upcoming/>
-                    </div>
-                </ScrollPage>
-           </ScrollContainer>
+                </div>
+            </div>
+            </Animated>
+            <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true} animationInDelay={6} animationInDuration={2000}>
+            <div className="container">
+                <Bishop/>
+            </div>
+            </Animated>
+            <div className="container">
+                <h1>Join us Sunday mornings as we gather online</h1>
+                <p>The Faith INternational Ministries Church wants to do everything we can to ensure that our communities are taking the necessary precautions to slow the spread of COVID-19. At the same time, we want to make it possible for everyone to experience the fellowship and Biblical teaching that keeps our faith strong, which is more important now than ever.
+                    With this in mind, we are ready to offer a series of special live stream worship services, Sundays at 11am GMT+3. These will take the place of in-person services for all of our Bay Area locations. We hope you’ll join us online!</p>
+                <br/><br/><a href="/Live">--- join us live</a>
+                <img src="https://source.unsplash.com/400x300/?Church" alt=""/>
+            </div>
+            <div className="container">
+                <Twitter/>
+            </div>
+            <div className="container">
+                <Chairman/>
+            </div>
+            
+            <div className="container">
+                <Upcoming/>
+            </div>
+            <div className="container">
+                <Upcoming/>
+            </div>
+           
         </div>
     )
-    }
+}
