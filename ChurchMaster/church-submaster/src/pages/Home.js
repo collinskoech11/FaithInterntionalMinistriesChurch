837a7ca6df24a3b9383514a6ff7c5f52633e14a6
collinskoech11 <collinskoechck34@gmail.com>
2021-02-04 12:04:00 +0300

    aniation update

--- ChurchMaster/church-submaster/src/pages/Home.js
+++ ChurchMaster/church-submaster/src/pages/Home.js
@@ -10,8 +10,13 @@ import {Animated} from 'react-animated-css';
 import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, Move, MoveIn, MoveOut, Sticky, StickyIn, ZoomIn } from "react-scroll-motion";
 
 
-
 export default function Home() {
+
+    const Presenter = () => {
+        const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+        const FadeUp = batch(Fade(), Move(), Sticky());
+      
+
     return (
         <div>
             <Animated animationIn="bounceIn" animationOut="fadeOut" isVisible={true}>
@@ -50,31 +55,38 @@ export default function Home() {
             </div>
             </Animated>
             <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true} animationInDelay={6} animationInDuration={2000}>
-            <div className="container">
-                <Bishop/>
-            </div>
+                <div className="container">
+                    <Bishop/>
+                </div>
             </Animated>
-            <div className="container">
-                <h1>Join us Sunday mornings as we gather online</h1>
-                <p>The Faith INternational Ministries Church wants to do everything we can to ensure that our communities are taking the necessary precautions to slow the spread of COVID-19. At the same time, we want to make it possible for everyone to experience the fellowship and Biblical teaching that keeps our faith strong, which is more important now than ever.
-                    With this in mind, we are ready to offer a series of special live stream worship services, Sundays at 11am GMT+3. These will take the place of in-person services for all of our Bay Area locations. We hope you’ll join us online!</p>
-                <br/><br/><a href="/Live">--- join us live</a>
-                <img src="https://source.unsplash.com/400x300/?Church" alt=""/>
-            </div>
-            <div className="container">
-                <Twitter/>
-            </div>
-            <div className="container">
-                <Chairman/>
-            </div>
-            
-            <div className="container">
-                <Upcoming/>
-            </div>
-            <div className="container">
-                <Upcoming/>
-            </div>
-           
+            <ScrollContainer>
+                <ScrollPage>
+                    <div className="container">
+                        <h1>Join us Sunday mornings as we gather online</h1>
+                        <p>The Faith INternational Ministries Church wants to do everything we can to ensure that our communities are taking the necessary precautions to slow the spread of COVID-19. At the same time, we want to make it possible for everyone to experience the fellowship and Biblical teaching that keeps our faith strong, which is more important now than ever.
+                            With this in mind, we are ready to offer a series of special live stream worship services, Sundays at 11am GMT+3. These will take the place of in-person services for all of our Bay Area locations. We hope you’ll join us online!</p>
+                        <br/><br/><a href="/Live">--- join us live</a>
+                        <img src="https://source.unsplash.com/400x300/?Church" alt=""/>
+                    </div>
+                </ScrollPage>
+                <ScrollPage>
+                    <div className="container">
+                        <Twitter/>
+                    </div>
+                </ScrollPage>
+                <ScrollPage>
+                    <div className="container">
+                        <Chairman/>
+                    </div>
+                </ScrollPage>
+                    <div className="container">
+                        <Upcoming/>
+                    </div>
+                    <div className="container">
+                        <Upcoming/>
+                    </div>
+           </ScrollContainer>
         </div>
     )
+    }
 }
