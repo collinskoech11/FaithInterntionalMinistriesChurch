f0c4471e1f7131b17381e54daff9ce1ebd91783a
collinskoech11 <collinskoechck34@gmail.com>
2021-02-07 18:48:48 +0300

    Added FaithFacebookPage.js

---
+++ ChurchMaster/church-submaster/src/Navigators/FaithFacebookPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./style.css";
+
+function FaithFacebookPage() {
+    return (
+        <div style={{width: 1000}}>
+            <div class="fb-page" 
+                data-href="https://www.facebook.com/Faith-Ministries-and-churches-international-1557669224500386/"
+                data-tabs="timeline,events,messages"
+                data-width="990" 
+                data-hide-cover="false"
+                data-show-facepile="false">
+            </div>
+        </div>
+    )
+}
+
+export default FaithFacebookPage
